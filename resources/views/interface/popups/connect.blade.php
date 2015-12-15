10c14cb7bc028d01dd336946978dbd54af258679
Mubashwer Khurshid <mskh@student.unimelb.edu.au>
2015-12-15 06:04:01 +0000

    added password matching

--- resources/views/interface/popups/connect.blade.php
+++ resources/views/interface/popups/connect.blade.php
@@ -43,6 +43,10 @@
       						<label>Password</label>
       						<input type="password" name="password" class="form-control" placeholder="password" minlength="6" required>
       					</div>
+                        <div class="form-group">
+                            <label>Confirm Password</label>
+                            <input type="password" name="password-confirm" id="password-confirm" class="form-control" placeholder="password" data-parsley-equalto="#password" required>
+                        </div>
     						<input type="hidden" name="referrer" class="form-control">
       					<!-- <div class="form-group">
       						<label>Check</label>
